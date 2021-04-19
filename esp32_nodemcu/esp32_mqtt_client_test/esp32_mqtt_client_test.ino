2a47f6a818a4291de68dda245628db0dcf04997c
Junsang Lee <mjnhb2001@gmail.com>
2021-04-19 09:36:50 +0900

    아두이노 양방향 출입 카운터 + mqtt 통신

--- esp32_nodemcu/esp32_mqtt_client_test/esp32_mqtt_client_test.ino
+++ esp32_nodemcu/esp32_mqtt_client_test/esp32_mqtt_client_test.ino
@@ -1,7 +1,13 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <sstream>
+#include <Wire.h>
 
+#define sensorPin1 36
+#define sensorPin2 13 
+
+//////////////////////////////////////
+///////network and mqtt setup/////////
 const char *ssid = "iptime";
 const char *password = "89652097";
 const char *mqtt_server = "192.168.0.26";
@@ -9,14 +15,25 @@ const int mqttPort = 1883;
 const char *mqtt_user = "martini";
 const char *mqtt_password = "martini";
 
-const char *topic_pub = "/mac_address";
-
-byte mac[6];
-
+byte mac[7] = { '\0', };
 #define UPDATE_INTERVAL 50000L
+///////////////////////////////////////
+//////////IR sensor setup//////////////
+int count = 0;
+
+boolean sensor1Triggered = false;
+unsigned long sensor1Time;
+byte currState1;
+byte prevState1 = HIGH;
+
+boolean sensor2Triggered = false;
+unsigned long sensor2Time;
+byte currState2;
+byte prevState2 = HIGH;
+byte message[10] = { '\0', };
+//////////////////////////////////////
 
 WiFiClient espClient;
-
 PubSubClient mqttClient(espClient);
 
 void setup() {
@@ -24,6 +41,8 @@ void setup() {
   WiFi.macAddress(mac);
   Serial.begin(115200);
   WiFi.begin(ssid, password);
+  pinMode(sensorPin1, INPUT);
+  pinMode(sensorPin2, INPUT);
 
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -46,14 +65,60 @@ void setup() {
     }
   }
 
-  mqttClient.publish(topic_pub, mac, 6);
+  mqttClient.publish("/init", mac, 7);
+  mqttClient.publish("/create_all", mac, 7);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  mqttClient.loop();
+  currState1 = digitalRead(sensorPin1);
+  if(currState1 != prevState1) {
+    if(currState1 == LOW) {
+      sensor1Triggered = true;
+      sensor1Time = millis();
+    }
+  }
 
-  mqttClient.publish(topic_pub, mac, 6);
+  currState2 = digitalRead(sensorPin2);
+  if(currState2 != prevState2) {
+    if(currState2 == LOW) {
+      sensor2Triggered = true;
+      sensor2Time = millis();
+    }
+  }
+
+  if(sensor2Triggered && sensor1Triggered) {
+    if(sensor1Time > sensor2Time) {
+      count++;
+      Serial.print("Count : ");
+      Serial.println(count);
+      memset(message, '\0', 10);
+      memcpy(message, mac, 6);
+      
+      message[8] = (byte)count;
+      mqttClient.publish("/room_all_add", message, 10);
+      memset(message, '\0', 10);
+      delay(200);
+    }
+    if(sensor2Time > sensor1Time) {
+      count--;
+      if(count <= 0) count = 0;
+      Serial.print("Count : ");
+      Serial.println(count);
+      memset(message, '\0', 10);
+      memcpy(message, mac, 6);
+      
+      message[8] = (byte)count;
+      mqttClient.publish("/room_all_sub", message, 10);
+      memset(message, '\0', 10);
+      delay(200);
+    }
+    sensor1Triggered = false;
+    sensor2Triggered = false;
+  }
+//  mqttClient.loop();
+//
+//  mqttClient.publish(topic_pub, mac, 6);
   
 
 }
